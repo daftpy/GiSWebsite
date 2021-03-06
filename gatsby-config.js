469e69dcabfd84a3b26bfd687dcfb89d082da4d1
Alexander Flood <daftpy@gmail.com>
2021-03-06 00:58:36 -0600

    Fixed markdown configuration.

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/src/pages`,
+        path: `${__dirname}/_posts/blog`,
         name: 'pages',
       },
     },
