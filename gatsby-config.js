5d69da4b3d2aea5dcc8accb0ab8558461a5815f1
Alexander Flood <daftpy@gmail.com>
2021-03-06 00:36:37 -0600

    Added Netlify CMS and configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: "Gabe is Gunk",
   },
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-styled-components"],
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-styled-components",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 };
